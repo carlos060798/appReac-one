06acc5911345e891ef5e4ed2c0542e50b0eac3f9
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-03-19 13:20:30 -0500

    version 2

--- client/src/PAGES/HomePages.js
+++ client/src/PAGES/HomePages.js
@@ -1,7 +1,29 @@
+import { Container, Row, Col ,Button} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import rutas from "../help/rutas";
 export default function AppHome(){
     
     return (
-      <div>soy tu inicio bay</div>
+      <Container>
+      <Row className="mt-5">
+        <Col xs={{span:12}} md={{span:6}} className="mb-5 text-center">
+        
+       
+        <h2 >Bienvenido al gestor de tareas</h2>
+        <p> 
+          gestiona tus tareas en nuestro gestor
+        
+        </p>
+        <div>
+        <Link to={rutas.LoginPages}>Ingresa</Link> 
+         <Button as={Link} to={rutas.RegisterPages} className="ml-1 ms-2 ">Crea una cuenta</Button>
+        </div>
+        </Col>
+        <Col>
+         <img  className="img-fluid" src="/img/task-manager.svg" alt="imagen" />
+        </Col>
+      </Row>
+    </Container>
     );
   
   
--- client/src/PAGES/NotPages.js
+++ client/src/PAGES/NotPages.js
@@ -1,9 +1,20 @@
-export default function AppNotPages(){
-    
-    return (
-      <div>ERROR 404</div>
-    );
-  
-  
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import rutas from "../help/rutas";
 
+export default function AppNotPages() {
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col md={{span:4, offset:4}} className="text-center">
+          <img style={{width:'100%'}} src="/img/404-not-found.svg"  alt="una imagen"/>
+       
+        <h2>Pagina no encontrada</h2>
+        <p>
+        <Link to={rutas.HomePages}>Inicio</Link>
+        </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
