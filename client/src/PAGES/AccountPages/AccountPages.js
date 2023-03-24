d55913b37e4831a4bc39fe7e59315c9e29bdadc5
Carlos Danilo Angarita Garcia <82780561+carlos060798@users.noreply.github.com>
2023-03-23 21:46:04 -0500

    version 4

---
+++ client/src/PAGES/AccountPages/AccountPages.js
@@ -0,0 +1,46 @@
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import useAuth from "../../user/useAuth";
+export default function AppAccount() {
+  const { user } = useAuth();
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col xs={12} className="text-center">
+          <img
+            src="/img/male_avatar.svg"
+            alt="soy tu cuenta"
+            style={{ width: "200px", height: "200px", borderRadius: "50%" }}
+          />
+        </Col>
+        <Col className="mt-4">
+          <Card style={{maxWidth:"350px"}} className="mx-auto p-4" >
+            <p className="text-center">
+              <b>nombre:</b>
+              {user.name}
+            </p>
+
+            <p className="text-center">
+              <b>correo:</b>
+              {user.email}
+            </p>
+
+            <p className="text-center">
+              <b>rol:</b>
+              {user.role}
+            </p>
+
+            <Button variant="warning" className="mt-1" >
+              Editar cuenta
+            </Button>
+            <Button variant="link" className="mt-2" >
+              Editar contraseña
+            </Button>
+            <Button variant="link" className="mt-3 text-danger">
+              Eliminar cuenta
+            </Button>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
